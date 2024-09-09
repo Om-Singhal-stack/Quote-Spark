92ab7d3c694107bdbbef98424ae4571e62872d8b
Om Singahl <142425555+Om-Singhal-stack@users.noreply.github.com>
2024-09-09 20:00:55 +0000

    q5

--- logic.js
+++ logic.js
@@ -15,8 +15,11 @@ async function mycall() {
 		// console.log(result.quote.quote);
 		const container = document.querySelector("#main_quotes");
 		const text = document.createElement("p");
+		const auth_text = document.createElement("p");
 		text.innerText = result.quote.quote;
+		auth_text.innerText = result.quote.author;
 		container.appendChild(text);
+		container.appendChild(auth_text);
 	} catch (error) {
 		console.error(error);
 	}
